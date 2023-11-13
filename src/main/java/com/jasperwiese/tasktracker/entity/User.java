fb870dea4fdc7b57926c89802cb284ae9de2d107
jasperwiese <jasper.wiese@protonmail.com>
2023-11-13 15:52:25 +0200

    added Repository interfaces

--- src/main/java/com/jasperwiese/tasktracker/entity/User.java
+++ src/main/java/com/jasperwiese/tasktracker/entity/User.java
@@ -1,2 +1,23 @@
-package com.jasperwiese.tasktracker.entity;public class User {
+package com.jasperwiese.tasktracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String mobile;
+    private String password;
 }
